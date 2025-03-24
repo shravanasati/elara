dfcb07f9749ce3c5e7dc4ee3f64fef20b0ba042d
Shravan Asati <dev.shravan@protonmail.com>
2025-03-24 21:53:56 +0530

    minor changes

---
+++ elara/template.py
@@ -0,0 +1,23 @@
+from jinja2 import FileSystemLoader, Environment, select_autoescape
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class RenderOptions:
+    filename: str
+    cells: 
+
+
+class TemplateRenderer:
+    def __init__(self):
+        self.__env = Environment(
+            loader=FileSystemLoader("templates"),
+            auto_reload=False,
+            autoescape=select_autoescape(),
+        )
+
+        self.__template = self.__env.get_template("export.html")
+
+    def render(self, options: RenderOptions)
+        return self.__template.render(**options.as_dict())
+
+
